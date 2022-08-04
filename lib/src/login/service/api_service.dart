6b5773c39cdfd62bde676767656be2050bc8f104
Bá Nguyễn <luutru.banguyen@gmail.com>
2022-08-04 14:30:45 +0700

    api

--- lib/src/login/service/api_service.dart
+++ lib/src/login/service/api_service.dart
@@ -9,7 +9,23 @@ class ApiService {
     required String pass,
   }) async {
     try {
-      final Response response = await _dio.post('$baseUrl${params['login']}');
+      final Response response = await _dio.post('$baseUrl${params['login']}',
+          data: {"phone": phone, "pass": pass});
+      return response;
+    } catch (e) {
+      print('error request :$e');
+      return null;
+    }
+  }
+
+  Future<Response?> postSignup({
+    required String phone,
+    required String email,
+    required String pass,
+  }) async {
+    try {
+      final Response response =
+          await _dio.post('$baseUrl${params['signup']}', data: {});
       return response;
     } catch (e) {
       print('error request :$e');
