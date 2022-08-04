4c80c293769c2416c54d50cefec209a700c84cab
canhbui123 <91304359+canhbui123@users.noreply.github.com>
2022-08-04 16:08:37 +0700

    cubit and index

---
+++ lib/src/home/cubit/home_state.dart
@@ -0,0 +1,14 @@
+part of 'home_cubit.dart';
+
+abstract class HomeState extends Equatable {
+  const HomeState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class HomeInitial extends HomeState {}
+class HomeLoading extends HomeState {}
+class HomeLoaded extends HomeState {}
+class HomeError extends HomeState {}
+
